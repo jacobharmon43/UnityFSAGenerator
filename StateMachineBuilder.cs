61b651091e4f616d322cd18fee403606e532c941
jacobharmon43 <64942817+jacobharmon43@users.noreply.github.com>
2024-03-06 22:58:15 -0600

    Update StateMachineBuilder.cs

--- StateMachineBuilder.cs
+++ StateMachineBuilder.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System;
-using System.Linq;
 
 namespace FSA
 {
@@ -22,103 +21,75 @@ namespace FSA
      *                          .WithState("Y")
      *                          .WithOnEnter( () => { foobar(); } )
      *                          .Build()
-     * See ReadMe for a more flushed out example.
      */
 
 
 
     public class StateMachineBuilder
     {
-        private readonly List<string> _states = new List<string>();
-        private readonly Dictionary<string, List<Action>> _onEnter = new Dictionary<string, List<Action>>();
-        private readonly Dictionary<string, List<Action>> _onExit = new Dictionary<string, List<Action>>();
-        private readonly Dictionary<string, List<Action>> _onRun = new Dictionary<string, List<Action>>();
-        private readonly List<(string from, string to, Func<bool> condition)> _transitions = new List<(string from, string to, Func<bool> condition)>();
-        private readonly List<(string to, Func<bool> condition)> _anyStates = new List<(string to, Func<bool> condition)>();
+        private readonly List<string> _states = new();
+        private readonly Dictionary<string, List<Action>> _onEnter = new();
+        private readonly Dictionary<string, List<Action>> _onExit = new();
+        private readonly Dictionary<string, List<Action>> _onRun = new();
+        private readonly Dictionary<string, List<(string, Func<bool>)>> _transitions = new();
         private string _currentState;
 
-        public StateMachineBuilder WithState(string name)
-        {
+        public StateMachineBuilder WithState(string name) {
             if (_states.Contains(name)) throw new ArgumentException($"State {name} already exists.");
             _states.Add(name);
             _currentState = name;
             return this;
         }
 
-        public StateMachineBuilder WithTransitionFromAnyState(Func<bool> condition)
-        {
-            _anyStates.Add((_currentState,condition));
+        public StateMachineBuilder WithOnEnter(Action onEnter) {
+            TryAddToDictionary(_onEnter, _currentState, onEnter);
             return this;
         }
 
-        public StateMachineBuilder WithOnEnter(Action onEnter)
-        {
-            if(_onEnter.ContainsKey(_currentState)){
-                _onEnter[_currentState].Add(onEnter);
-            }
-            else{
-                _onEnter.Add(_currentState, new List<Action>(){onEnter});
-            }
+        public StateMachineBuilder WithOnRun(Action onRun) {
+            TryAddToDictionary(_onRun, _currentState, onRun);
+            return this;
+        }
+        
+        public StateMachineBuilder WithOnExit(Action onExit) {
+            TryAddToDictionary(_onExit, _currentState, onExit);
             return this;
         }
 
-        public StateMachineBuilder WithOnExit(Action onExit)
-        {
-            if(_onExit.ContainsKey(_currentState)){
-                _onExit[_currentState].Add(onExit);
-            }
-            else{
-                _onExit.Add(_currentState, new List<Action>(){onExit});
-            }
+        public StateMachineBuilder WithTransition(string nextState, Func<bool> condition) {
+            TryAddToDictionary(_transitions, _currentState, (nextState, condition));
             return this;
         }
 
-        public StateMachineBuilder WithOnRun(Action onRun)
-        {
-            if(_onRun.ContainsKey(_currentState)){
-                _onRun[_currentState].Add(onRun);
-            }
-            else{
-                _onRun.Add(_currentState, new List<Action>(){onRun});
-            }
-            return this;
-        }
-
-        public StateMachineBuilder WithTransition(string nextState, Func<bool> condition)
-        {
-            _transitions.Add((_currentState, nextState, condition));
-            return this;
-        }
-
-        public StateMachine Build()
-        {
-            var statesLookup = _states.Select(name => new State(name, GetOnEnter(name), GetOnRun(name), GetOnExit(name)))
-              .ToDictionary(x => x.Name, x => x);
-            foreach (var (from, to, condition) in _transitions.Select(t => (statesLookup[t.from], statesLookup[t.to], t.condition)))
-            {
-                from.AssignTransition(to, condition);
-            }
-
-            StateMachine sm = new StateMachine(statesLookup.Values.First());
-            foreach(var (to, condition) in _anyStates.Select(t=> (statesLookup[t.to], t.condition)))
-            {
-                sm.AddAnyState(new Transition(condition, to));
-            }
-
-            return sm;
-
-            Action GetOnRun(string key) => ConvertList(GetAction(_onRun, key));
-            Action GetOnExit(string key) => ConvertList(GetAction(_onExit, key));
-            Action GetOnEnter(string key) => ConvertList(GetAction(_onEnter, key));
-            List<Action> GetAction(Dictionary<string, List<Action>> collection, string key) => collection.TryGetValue(key, out var value) ? value : null;
-
-            Action ConvertList(List<Action> l){
-                Action a = null;
-                if(l == null) return a;
-                foreach(Action b in l){
-                    a += b;
+        public StateMachine Build() {
+            StateMachine sm = new StateMachine();
+            foreach (string state in _states) {
+                State s = new(state, ConsolidateActions(_onEnter.GetValueOrDefault(state)), ConsolidateActions(_onRun.GetValueOrDefault(state)), ConsolidateActions(_onExit.GetValueOrDefault(state)));
+                if (_transitions.ContainsKey(state)) {
+                    foreach ((string, Func<bool>) transition in _transitions[state]) {
+                        s.AssignTransition(new Transition(transition.Item1, transition.Item2));
+                    }
                 }
-                return a;
+                sm.AddState(s);
+            }
+            sm.SetStartingState(_states[0]);
+            return sm;
+        }
+
+        Action ConsolidateActions(List<Action> l) {
+            Action allActions = null;
+            if (l == null) return allActions;
+            foreach (Action action in l) {
+                allActions += action;
+            }
+            return allActions;
+        }
+
+        private void TryAddToDictionary<T>(Dictionary<string, List<T>> dictionary, string key, T value) {
+            if (dictionary.ContainsKey(key)) {
+                dictionary[key].Add(value);
+            } else {
+                dictionary.Add(key, new List<T>() { value });
             }
         }
     }
