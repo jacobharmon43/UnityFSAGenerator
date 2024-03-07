6806435cd41a3d424a899cb27e77f649aca81a63
jacobharmon43 <64942817+jacobharmon43@users.noreply.github.com>
2024-03-06 22:57:39 -0600

    Update State.cs

--- State.cs
+++ State.cs
@@ -1,73 +1,51 @@
 using System;
 using System.Collections.Generic;
 
-namespace FSA
-{
-    /* How to use:
-     * 
-     * The only thing you need to really know, outside of how to construct it
-     * is that the transitions list is order-important, so order things in terms
-     * of their importance. Or dont allow multiple transitions to be true at once via more complex
-     * checks
-     * 
-     */
-
-    public class State
-    {
-
+namespace FSA {
+    public class State {
         public string Name { get; private set; }
-        private Action enterState;
-        private Action exitState;
-        private Action duringState;
+        private readonly Action _onEnter;
+        private readonly Action _onRun;
+        private readonly Action _onExit;
+        private readonly List<Transition> _transitions = new List<Transition>();
 
-        private List<Transition> transitions;
-
-        public State(string name, Action enter, Action during, Action exit)
-        {
-            this.Name = name;
-            this.enterState = enter;
-            this.duringState = during;
-            this.exitState = exit;
-            this.transitions = new List<Transition>();
+        public State(string name, Action enter, Action during, Action exit) {
+            Name = name;
+            _onEnter = enter;
+            _onRun = during;
+            _onExit = exit;
+            _transitions = new List<Transition>();
         }
 
-        public void AssignTransition(State toState, Func<bool> condition)
-        {
-            Transition t = new Transition(condition, toState);
-            transitions.Add(t);
+        public void AssignTransition(Transition t) {
+            _transitions.Add(t);
         }
 
-        public int Count(){
-            return transitions.Count;
+        public void InitializeState() {
+            _onEnter?.Invoke();
         }
 
-        public void InitializeState()
-        {
-            enterState?.Invoke();
+        public void OnExit() {
+            _onExit?.Invoke();
         }
 
-        public void OnExit()
-        {
-            exitState?.Invoke();
+        public void OnUpdate() {
+            _onRun?.Invoke();
         }
 
-        public void OnUpdate()
-        {
-            duringState?.Invoke();
-        }
-
-        public State QueryStateChange() //Queries all transitions attached to this state, if any func<bool> result in true, the first one found is returned
-        {
-            foreach(Transition t in transitions)
-            {
-                if (t.Check())
-                {
+        public string QueryStateChange() {
+            foreach(Transition t in _transitions) {
+                if (t.Check()) {
                     return t.NextState;
                 }
             }
-            return this;
+            return Name;
         }
         
         public static implicit operator bool(State s) => s != null;
+        public static bool operator ==(string s, State state) => s == state.Name;
+        public static bool operator !=(string s, State state) => s != state.Name;
+        public static bool operator ==(State state, string s) => state.Name == s;
+        public static bool operator !=(State state, string s) => state.Name != s;
     }
 }
