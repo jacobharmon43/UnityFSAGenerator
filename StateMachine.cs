25f6dbef1c33646f57919b7f75b24045244a348c
jacobharmon43 <64942817+jacobharmon43@users.noreply.github.com>
2024-03-06 22:58:00 -0600

    Update StateMachine.cs

--- StateMachine.cs
+++ StateMachine.cs
@@ -1,69 +1,75 @@
+using System;
 using System.Collections.Generic;
 
-namespace FSA
-{
-    /* How to use:
-     * 
-     * Once you have set up the statemachine either by meticulously constructing it, or using the builder I wrote
-     * Just run RunStateMachine() in the Update() method, or wherever you want it to run. It handles all the logic otherwise
-     * itself
-     * 
-     */
-
-    public class StateMachine
-    {
+namespace FSA {
+    public class StateMachine {
         public State CurrentState { get; private set; }
+        public float TimeInCurrentState { get; private set; }
+
+        private DateTime _timeSinceLastUpdate;
+        private readonly Dictionary<string, State> _states = new Dictionary<string, State>();
         private State _startingState;
+        private bool _paused = false;
+        private bool _initialized = false;
 
-        private List<Transition> _anyStates = new List<Transition>();
+        public StateMachine() {}
 
-        public StateMachine(State startingState)
-        {
-            CurrentState = startingState;
-            _startingState = startingState;
-            CurrentState.InitializeState();
+        public void AddState(State s) {
+            _states.Add(s.Name, s);
         }
 
-        public void ResetStateMachine(){
+        public void SetStartingState(string state) {
+            _startingState = _states[state];
+        }
+
+        /// <summary>
+        /// Also can be used to reset the state machine
+        /// </summary>
+        public void StartStateMachine() {
             CurrentState = _startingState;
+            CurrentState.InitializeState();
+            _timeSinceLastUpdate = DateTime.Now;
+            _initialized = true;
+            _paused = false;
         }
 
-        public void RunStateMachine()
-        {
+        public void PauseStateMachine() => _paused = true;
+        public void UnpauseStateMachine() => _paused = false;
+
+        public void RunStateMachine() {
+            // Realistically this should be on the user to do, but its here for convenience and backwards compatibility
+            if (!_initialized) { StartStateMachine(); }
+            if (_paused) return;
             CurrentState.OnUpdate();
-            State tmp = QueryStateChange();   
-            if(tmp && tmp != CurrentState)
-            {
+            string tmp = QueryStateChange();   
+            if(tmp != CurrentState) {
                 ChangeState(tmp);
             }
+            DateTime currentTime = DateTime.Now;
+            TimeSpan deltaTime = currentTime - _timeSinceLastUpdate;
+            TimeInCurrentState = (float)deltaTime.TotalSeconds;
         }
 
-        public void AddAnyState(Transition t)
-        {
-            _anyStates.Add(t);
+        /// <summary>
+        /// Public facing method to change the state whenever, used to override statemachine logic if needed
+        /// </summary>
+        /// <param name="exitCurrentState"> If true, current states onExit method is called. Otherwise just load the next state </param>
+        public void SetState(string stateName, bool exitCurrentState = false) {
+            ChangeState(stateName, exitCurrentState);
         }
 
-        private State QueryStateChange()
-        {
-            foreach(Transition t in _anyStates)
-            {
-                if(t.Check())
-                {
-                    return t.NextState;
-                }
+        private string QueryStateChange() {
+            return CurrentState.QueryStateChange();
+        }
+
+        private void ChangeState(string nextState, bool exitCurrentState = true) {
+            if (exitCurrentState) {
+                CurrentState.OnExit();
             }
-            return CurrentState.QueryStateChange();;
-        }
-
-        private void ChangeState(State nextState) //Calls exit methods and initialization methods of new machine
-        {
-            if (CurrentState == nextState || nextState == null)
-            {
-                return;
-            }
-            CurrentState.OnExit();
-            CurrentState = nextState;
+            CurrentState = _states[nextState];
             CurrentState.InitializeState();
+            _timeSinceLastUpdate = DateTime.Now;
+            TimeInCurrentState = 0;
         }
     }
 }
